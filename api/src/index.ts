5b34270ed7b5f1d1cf7b4e626045ccc8735cdb38
Ardit Daku <arditdaku1@gmail.com>
2021-07-22 11:54:43 +0200

    Setup Api Endpoints

--- api/src/index.ts
+++ api/src/index.ts
@@ -9,5 +9,26 @@ app.use('/', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ data: 'Hello World' });
 });
 
+/**
+ *Get
+ * Get all lists
+ */
+app.get('/lists', (req: Request, res: Response) => {});
+/**
+ * Post
+ * Create a list
+ */
+app.post('/lists', (req: Request, res: Response) => {});
+/**
+ * Put
+ * Update a list
+ */
+app.put('/lists/:id', (req: Request, res: Response) => {});
+/**
+ * Delete
+ * Delete a list
+ */
+app.delete('/lists/:id', (req: Request, res: Response) => {});
+
 // Start server
 app.listen(port, () => console.log(`Server is listening on port ${port}!`));
